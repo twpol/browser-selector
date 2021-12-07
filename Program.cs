eea48b2db2e5440363e2abbf6a297208830b641a
James Ross <git@james-ross.co.uk>
2021-12-07 15:19:22 +0000

    feat: Initial implementation of opening correct browser

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Microsoft.Extensions.Configuration;
 
 namespace Browser_Selector
 {
@@ -6,7 +11,49 @@ namespace Browser_Selector
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var (command, arguments) = SplitCommandArgument(Environment.CommandLine);
+
+            var executableFilePath = Process.GetCurrentProcess().MainModule.FileName;
+            var configFilePath = Path.Combine(Path.GetDirectoryName(executableFilePath), "config.json");
+            var config = new ConfigurationBuilder()
+                .AddJsonFile(configFilePath)
+                .Build();
+
+            foreach (var browser in config.GetChildren().OrderBy(browser => -(browser["Url"] ?? "").Length))
+            {
+                var hostname = new Regex(browser["Hostname"] ?? ".");
+                if (!hostname.IsMatch(Environment.MachineName))
+                {
+                    continue;
+                }
+
+                var url = new Regex(browser["Url"] ?? ".");
+                var isMatch = args.Any(arg => url.IsMatch(arg));
+                if (!isMatch)
+                {
+                    continue;
+                }
+
+                Console.WriteLine($"Launching with {browser.Key}...");
+
+                var (launchCommand, launchArguments) = SplitCommandArgument($"{browser["Launch"]} {arguments}");
+                Process.Start(launchCommand, launchArguments);
+                break;
+            }
+        }
+
+        static (string, string) SplitCommandArgument(string commandLine)
+        {
+            if (commandLine[0] == '"')
+            {
+                var argumentsStart = commandLine.IndexOf('"', 1) + 1;
+                return (commandLine.Substring(1, argumentsStart - 2), commandLine.Substring(argumentsStart).Trim());
+            }
+            else
+            {
+                var argumentsStart = commandLine.IndexOf(' ');
+                return (commandLine.Substring(0, argumentsStart), commandLine.Substring(argumentsStart).Trim());
+            }
         }
     }
 }
